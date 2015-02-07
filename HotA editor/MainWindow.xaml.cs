b63bc59ed7d2c45841d5a992b665bf7cffe6841a
sake12 <sake12@gmail.com>
2015-02-07 21:43:55 +0100

    Done file decoding part...

--- HotA editor/MainWindow.xaml.cs
+++ HotA editor/MainWindow.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace HotA_editor
 {
@@ -26,76 +16,89 @@ namespace HotA_editor
             InitializeComponent();
         }
 
-        const string FileName = @"E:\Gry\DUPA!!!!\HotA.dat";
+        const string FileName = "HotA.dat";
 
         void DisplayValues()
         {
             if (!File.Exists(FileName)) return;
-            using (var reader = new BinaryReader(File.Open(FileName, FileMode.Open)))
+            using (var read = new BinaryReader(File.Open(FileName, FileMode.Open), Encoding.ASCII))
             {
-                var dupa = reader;
+                var reader = read;
 
-                var s = new string(dupa.ReadChars(4));
-                if (s != "HDAT")
+                var s = ReadString(ref reader, 4) + reader.ReadInt32();
+                if (s != "HDAT2")
                     return;
 
-                dupa.ReadChars(8);
+                var noOfFiles = reader.ReadInt32();
 
-                const int ileBohaterow = 62;
-
-                for (var i = 0; i < ileBohaterow; i++)
+                for (var i = 0; i < noOfFiles; i++)
                 {
-                    Console.WriteLine(i);
-                    var tmp = ReadString(ref dupa, dupa.ReadInt32());
-                    TextB.AppendText("Nazwa pliku:       " + tmp + Environment.NewLine);
-                    Console.WriteLine(tmp);
-                    TextB.AppendText("Katalog:           " + ReadString(ref dupa, dupa.ReadInt32()) + Environment.NewLine);
-                    TextB.AppendText("***pozostałe opcje***" + Environment.NewLine + ReadBytes(ref dupa, 8) + Environment.NewLine);
-                    TextB.AppendText("Ikona duża:        " + ReadString(ref dupa, dupa.ReadInt32()) + Environment.NewLine);
-                    TextB.AppendText("Ikona mała:        " + ReadString(ref dupa, dupa.ReadInt32()) + Environment.NewLine);
-                    TextB.AppendText("Spec. krótka:      " + ReadString(ref dupa, dupa.ReadInt32()) + Environment.NewLine);
-                    TextB.AppendText("Spec. długa:       " + ReadString(ref dupa, dupa.ReadInt32()) + Environment.NewLine);
-                    TextB.AppendText("Xxx1               " + ReadString(ref dupa, dupa.ReadInt32()) + Environment.NewLine);
-                    TextB.AppendText("Xxx2               " + ReadString(ref dupa, dupa.ReadInt32()) + Environment.NewLine);
-                    Console.WriteLine("xx3 = " + dupa.BaseStream.Position);
-                    TextB.AppendText("Xxx3               " + ReadString(ref dupa, dupa.ReadInt32()) + Environment.NewLine);
-                    Console.WriteLine("xx4 = " + dupa.BaseStream.Position);
-                    TextB.AppendText("Xxx4               " + ReadString(ref dupa, dupa.ReadInt32()) + Environment.NewLine);
-                    Console.WriteLine(dupa.BaseStream.Position.ToString());
-                    var tmp2 = ReadBoolean(ref dupa);
-                    TextB.AppendText("Dane Dodatkowe?    " + tmp2 + Environment.NewLine);
-                    if (tmp2)
+                    var tmp = ReadString(ref reader, reader.ReadInt32());
+                    //TextB.AppendText("Nazwa pliku:       " + tmp + Environment.NewLine);
+
+                    var tmp1 = ReadString(ref reader, reader.ReadInt32());
+                    TextB.AppendText("Katalog:           " + tmp1 + Environment.NewLine);
+
+                    var tmp2 = ReadBytes(ref reader, 8);
+                    //TextB.AppendText("***pozostałe opcje***" + Environment.NewLine + tmp2 + Environment.NewLine);
+
+                    var tmp3 = ReadString(ref reader, reader.ReadInt32());
+                    //TextB.AppendText("Ikona duża:        " + tmp3 + Environment.NewLine);
+
+                    var tmp4 = ReadString(ref reader, reader.ReadInt32());
+                    //TextB.AppendText("Ikona mała:        " + tmp4 + Environment.NewLine);
+
+                    var tmp5 = ReadString(ref reader, reader.ReadInt32());
+                    //TextB.AppendText("Spec. krótka:      " + tmp5 + Environment.NewLine);
+
+                    var tmp6 = ReadString(ref reader, reader.ReadInt32());
+                    //TextB.AppendText("Spec. długa:       " + tmp6 + Environment.NewLine);
+
+                    var tmp7 = ReadString(ref reader, reader.ReadInt32());
+                    //TextB.AppendText("Xxx1               " + tmp7 + Environment.NewLine);
+
+                    var tmp8 = ReadString(ref reader, reader.ReadInt32());
+                    //TextB.AppendText("Xxx2               " + tmp8 + Environment.NewLine);
+
+                    var tmp9 = ReadString(ref reader, reader.ReadInt32());
+                    //TextB.AppendText("Xxx3               " + tmp9 + Environment.NewLine);
+
+                    var tmp10 = ReadString(ref reader, reader.ReadInt32());
+                    //TextB.AppendText("Xxx4               " + tmp10 + Environment.NewLine);
+
+                    var tmp11 = ReadBoolean(ref reader);
+                    //TextB.AppendText("Dane Dodatkowe?    " + tmp11 + Environment.NewLine);
+                    if (tmp11)
                     {
-                        TextB.AppendText("Xxx6               " + ReadBytes(ref dupa, dupa.ReadInt32()) + Environment.NewLine);
+                        var tmp12 = ReadBytes(ref reader, reader.ReadInt32());
+                        //TextB.AppendText("Xxx6               " + tmp12 + Environment.NewLine);
                     }
-                    TextB.AppendText("Ile licz?          " + ReadInts(ref dupa, dupa.ReadInt32()) + Environment.NewLine);
 
-                    TextB.AppendText(Environment.NewLine + Environment.NewLine);
+                    var tmp13 = ReadInts(ref reader, reader.ReadInt32());
+                    //TextB.AppendText("Ile licz?          " + tmp13 + Environment.NewLine);
                 }
-
-                TextB.AppendText(dupa.BaseStream.Position.ToString());
             }
         }
 
-        private string ReadString(ref BinaryReader stream, int length)
+        private static string ReadString(ref BinaryReader stream, int length)
         {
             return new string(stream.ReadChars(length));
         }
 
-        private Boolean ReadBoolean(ref BinaryReader stream)
+        private static Boolean ReadBoolean(ref BinaryReader stream)
         {
             return stream.ReadBoolean();
         }
 
-        private string ReadBytes(ref BinaryReader stream, int length)
+        private static string ReadBytes(ref BinaryReader stream, int length)
         {
             return String.Concat(stream.ReadBytes(length).Select(b => b.ToString("X2") + " "));
         }
 
-        private string ReadInts(ref BinaryReader stream, int length)
+        private static string ReadInts(ref BinaryReader stream, int length)
         {
             var ret = string.Empty;
-            for (int i = 0; i < length; i++)
+            for (var i = 0; i < length; i++)
             {
                 ret += stream.ReadInt32() + " ";
             }
