1a4b0e04dacb384925ebb725ab638ba10ebb1ae0
mirracz <mirracz@gmail.com>
2024-10-11 02:18:09 +0200

    Language selection turned into radio buttons

---
+++ HotA editor/Controls/RadioMenuItem.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HotA_editor;
+
+public class RadioMenuItem : MenuItem
+{
+    public RadioMenuItem() 
+    { 
+        IsCheckable = true;
+        PreviewMouseLeftButtonUp += RadioMenuItem_PreviewMouseLeftButtonUp;
+    }
+
+    private void RadioMenuItem_PreviewMouseLeftButtonUp(object sender, RoutedEventArgs e)
+    {
+        if (IsChecked)
+        {
+            // prevents unchecking
+            e.Handled = true;
+        }
+    }
+
+    protected override void OnChecked(RoutedEventArgs e)
+    {
+        base.OnChecked(e);
+
+        if (Parent is MenuItem parent)
+        {
+            foreach (var child in parent.Items.OfType<RadioMenuItem>())
+            {
+                if (child != this)
+                {
+                    child.IsChecked = false;
+                }
+            }
+        }
+    }
+}
